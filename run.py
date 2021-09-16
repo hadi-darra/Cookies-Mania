2cc21b17d87fbafc4fea7834154ba5087f44493f
hadi-darra <h.aldarra@gmail.com>
2021-09-16 08:58:25 +0000

    correcting get sales data function

--- run.py
+++ run.py
@@ -64,11 +64,12 @@ def get_sales_data():
         data_str = input("Enter your data here:\n")
 
         sales_data = data_str.split(",")
-
+        
         if validate_data(sales_data):
             print("Data is valid!")
             break
-    return sales_data
+
+    return update_worksheet_sales(sales_data)
 
 def back_to_menu():
     """
@@ -106,15 +107,14 @@ def validate_data(values):
 
     return True
 
-def update_worksheet(data, worksheet):
+def update_worksheet_sales(sales_data):
     """
-    Receives a list of integers to be inserted into a worksheet
-    Update the relevant worksheet with the data provided
+    Update sales worksheet, add new row with the list data provided
     """
-    print(f"Updating {worksheet} worksheet...\n")
-    worksheet_to_update = SHEET.worksheet(worksheet)
-    worksheet_to_update.append_row(data)
-    print(f"{worksheet} worksheet updated successfully\n")
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(sales_data)
+    print("Sales worksheet updated successfully.\n")
 
 def calculate_surplus_data(sales_row):
     """
@@ -133,6 +133,23 @@ def calculate_surplus_data(sales_row):
         surplus_data.append(surplus)
 
     return surplus_data
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 def get_last_5_entries_sales():
     """
@@ -176,16 +193,7 @@ def main():
     contains all the functions for the program
     """
     start()
-    """
-    data = get_sales_data()
-    sales_data = [int(num) for num in data]
-    update_worksheet(sales_data, "sales")
-    new_surplus_data = calculate_surplus_data(sales_data)
-    update_worksheet(new_surplus_data, "surplus")
-    sales_columns = get_last_5_entries_sales()
-    stock_data = calculate_stock_data(sales_columns)
-    update_worksheet(stock_data, "stock")
-    """
+    
 
 print("   ___            _    _                                      _       ")
 print("  / __\___   ___ | | _(_) ___  ___    /\/\   __ _ _ __   __ _(_) __ _ ")
@@ -196,4 +204,3 @@ print("                                                                      ")
 print("-----------------Please choose the number of the service--------------")
 print("                                                                      ")
 main()
-
